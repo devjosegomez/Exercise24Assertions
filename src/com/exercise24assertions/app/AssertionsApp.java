be28d3f2920b9deee81674ed1fdf1436781f0535
devjosegomez <devjosegomez@gmail.com>
2019-10-23 16:33:28 -0500

    Example of Asserts

--- src/com/exercise24assertions/app/AssertionsApp.java
+++ src/com/exercise24assertions/app/AssertionsApp.java
@@ -3,22 +3,26 @@ package com.exercise24assertions.app;
 import java.util.Scanner;
 
 public class AssertionsApp {
-
+//Asserts are used in testing
 	public static void main(String[] args) {
 		int edad=0;
 		System.out.println("Eres mayor de edad? Introduce tu edad: ");
 		Scanner input = new Scanner(System.in);
 		edad = input.nextInt();
-		
-		assert edad<0: "Edad no valida";
+		System.out.println(isMayorEdad(edad));
+	}
+	
+	public static String isMayorEdad(int edad) {
+		assert edad>0: "Edad no valida, solo se aceptan valores positivos";
+		String cad="";
 		
 		if(edad<18) {
-			System.out.println("Menor de edad");
+			 cad = "Menor de edad";
 		}
 		if(edad>=18) {
-			System.out.println("Chavorruco");
+			cad =  "Chavorruco";
 		}
-
+		return cad;
 	}
 
 }
